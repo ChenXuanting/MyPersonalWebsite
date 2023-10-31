6597b0a84b3130e7ce5aad1085941f49ef2d39cf
Xuanting Chen <bldcxt@gmail.com>
2023-10-31 03:24:05 -0400

    updated portrait and scrolling logic

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,78 +1,91 @@
-import React from "react";
-import { Flex, VStack, Box } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { VStack , Box } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import portrait from "../images/portrait.jpg"
 import TypingEffect from "./TypingEffect";
 
 const LandingSection = () => {
+  const [opacity, setOpacity] = useState(0);
+  const [typingFinished, setTypingFinished] = useState(false);
+
+  const texts = [
+    "Hello, I am Theo! ",
+    "Welcome to my website."
+  ];
+
+  const totalTypingDelay = texts.reduce((total, text) => total + 120 * text.length, 0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTypingFinished(true);
+    }, totalTypingDelay);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      if (!typingFinished) {  // Prevent any effect if typing is not finished
+        e.preventDefault();
+        return;
+      }
+
+      if (opacity < 1) {
+        e.preventDefault(); // Prevent scrolling while the image is not fully shown
+
+        if (e.deltaY > 0) {  // Only process downward scrolls
+          setOpacity((prevOpacity) => Math.min(prevOpacity + 0.15, 1));
+        }
+      }
+
+      if (typingFinished && opacity >= 1) {
+        document.body.style.overflowY = "auto";  // Enable default scrolling
+        window.removeEventListener('wheel', handleScroll); // Remove the scroll listener
+      }
+    };
+
+    document.body.style.overflowY = "hidden";
+    window.addEventListener('wheel', handleScroll, { passive: false });
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, [opacity, typingFinished]);
+
   return (
   <FullScreenSection
     justifyContent="center"
     alignItems="center"
     isDarkBackground = {false}
     backgroundColor="white"
+    position="relative"
+    zIndex="0"
   >
-  <Flex width="100%" height="100%" padding={5} justifyContent="center"
-        alignItems="center">
-      <VStack
-        flex="4.8"
+    <Box
+      position="absolute"
+      top="0"
+      right="0"
+      bottom="0"
+      left="0"
+      backgroundImage={`url(${portrait})`}
+      backgroundSize="cover"  // Temporarily set this for troubleshooting
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      opacity={opacity}
+      zIndex="-1" // Adjust this from -1 to 0
+    />
+    <VStack
         spacing={5}
-        marginRight={5}
         height="100%"
         justifyContent="center"
         alignItems="center"
-      >
-        {/* <Box
-          width= "100%"
-          height= "100%"
-          display="flex"
-          justifyContent="center"
-          alignItems="flex-end"
-        >
-          <img
-            src={portrait}
-            alt="Theo"
-            style={{
-              width:'auto',
-              height:'100%',
-              borderRadius: '10px'
-            }}
-          />
-        </Box> */}
-        <Box
-            width="100%"
-            height="100%"
-            position="relative"  // Make the Box a reference point for absolute positioning
-        >
-            <img
-                src={portrait}
-                alt="Theo"
-                style={{
-                    width: 'auto',
-                    height: '60%',
-                    borderRadius: '10px',
-                    position: 'absolute',  // Absolute position the image
-                    right: 0,  // Place it on the far right
-                    top: '50%',  // Center it vertically
-                    transform: 'translateY(-50%)'  // Adjust for the image's height to truly center it
-                }}
-            />
-        </Box>
-
-      </VStack>
-
-      <VStack
-        flex="5.2"
-        spacing={5}
-        height="100%"
-        justifyContent="center"
-        alignItems="start"
-      >
-        <TypingEffect startDelay={0}>Hello, I am Theo! </TypingEffect>
-        <TypingEffect startDelay={120 * ("Hello, I am Theo! ".length + "Welco".length)}>I work with web apps.</TypingEffect>
-        <TypingEffect startDelay={120 * ("Hello, I am Theo! ".length + "I work with web applications.".length  + "Welc".length)}>I work with deep learning.</TypingEffect>
-      </VStack>
-  </Flex>
+    >
+        {texts.map((text, index) => (
+          <TypingEffect key={index} startDelay={120 * texts.slice(0, index).reduce((total, t) => total + t.length, 0)}>
+            {text}
+          </TypingEffect>
+        ))}
+    </VStack>
   </FullScreenSection>
 );
 }
